117c0879a3381b70325f10784264f15ec2c9bb52
Peter Stelzer <pete@bipper.net>
2023-06-08 19:36:49 -0700

    Added a spinner for page loads, added a Dockerfile.

--- hooks/useMenuItemContent.tsx
+++ hooks/useMenuItemContent.tsx
@@ -1,6 +1,7 @@
-import { MenuItemType } from "components/app";
+import { MenuItemType, LoadingState} from "components/app";
 import { useEffect, useState } from "react";
-import { PresentationStyle } from "components/app";
+
+
 
 
 export type ImageDetails = {
@@ -13,24 +14,31 @@ export type ImageDetails = {
 export type ImagePaneDetailsType = {
     content: string;
     images: ImageDetails[];
-  }  
+}  
+
 
 const useMenuItemContent = (selectedMenuItem:MenuItemType | undefined ) => {
     const [content, setContent] = useState<ImagePaneDetailsType | undefined>(undefined);
     const [currentImageIndex, setCurrentImageIndex] = useState<number>();
-
+    const [loadingState, setLoadingState] = useState<LoadingState>(LoadingState.Loading)
   
     useEffect(() => {
       const fetchContent = async () => {
-          const response:void | Response = await fetch("http://localhost:8080/api/menuItemContent?menuItemId="+selectedMenuItem?.id);
-          const content = await response.json();  
-          setContent(content);
-      }
+          setLoadingState(LoadingState.Loading)
+          try{
+            const response:void | Response = await fetch("http://localhost:8080/api/menuItemContent?menuItemId="+selectedMenuItem?.id);
+            const content = await response.json();  
+            setContent(content);
+            setLoadingState(LoadingState.Loaded);
+          } catch {
+            setLoadingState(LoadingState.Error)
+          }
+       }
       if (selectedMenuItem && selectedMenuItem.id){ 
         fetchContent();
       }
     }, [selectedMenuItem]);
-    return {content, setContent, currentImageIndex, setCurrentImageIndex};
+    return {content, setContent, currentImageIndex, setCurrentImageIndex, loadingState};
   
 }
 
