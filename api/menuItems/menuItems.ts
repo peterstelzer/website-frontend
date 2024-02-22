27a3b16a64d85c4ecd096fc57ac451e6f51231cb
Peter Stelzer <pete@bipper.net>
2024-02-21 18:42:44 -0800

    Troubleshooting attempts.

--- api/menuItems/menuItems.ts
+++ api/menuItems/menuItems.ts
@@ -5,8 +5,12 @@ import {ImagePaneDetailsType} from "../../hooks/useMenuItemContent";
 
 export async function getMenuItems(): Promise<MenuItemType[]> {
     const configUrl = process.env.NEXT_PUBLIC_CONFIG_URL ? process.env.NEXT_PUBLIC_CONFIG_URL : "http://localhost:8000";
-        const apiKey = process.env.NEXT_PUBLIC_BASIC_AUTH_CREDS ? process.env.NEXT_PUBLIC_BASIC_AUTH_CREDS : "N/A";
-    const response:void | Response = await fetch(configUrl + "/api/menuItems");
+    const apiKey = process.env.NEXT_PUBLIC_BASIC_AUTH_CREDS ? process.env.NEXT_PUBLIC_BASIC_AUTH_CREDS : "N/A";
+    const response:void | Response = await fetch(configUrl + "/api/menuItems", {
+        headers: {
+            "Authorization": "Basic " + apiKey
+        }
+    });
     return await response.json();
 }
 
@@ -14,6 +18,10 @@ export async function getMenuItemContent(selectedMenuItem: MenuItemType | undefi
     const configUrl = process.env.NEXT_PUBLIC_CONFIG_URL ? process.env.NEXT_PUBLIC_CONFIG_URL : "http://localhost:8000";
     const url = configUrl + "/api/menuItemContent?menuItemId="+selectedMenuItem?.id;
     const apiKey = process.env.NEXT_PUBLIC_BASIC_AUTH_CREDS ? process.env.NEXT_PUBLIC_BASIC_AUTH_CREDS : "N/A";
-    const response:void | Response = await fetch(url);
+    const response:void | Response = await fetch(url, {
+        headers: {
+            "Authorization": "Basic " + apiKey
+        }
+    });
     return await response.json();
 }
