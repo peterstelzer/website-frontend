2ab3f66d821e6996c85d25d08dd5a7d6b1cda3b2
Peter Stelzer <pete@bipper.net>
2025-01-12 16:39:28 -0800

    Hide the version id on the url if we're browsing the published version

--- src/app/menuId/[menuId]/page.tsx
+++ src/app/menuId/[menuId]/page.tsx
@@ -34,7 +34,7 @@ export default function Page({ params }: Readonly<{ params: { menuId: string } }
         // if we are at a currentMenuItemId of 0 (if we just arrived here from the main domain name) or if we changed versions and we have a new home page id (and the current menu id is invalid now),
         // then find the menu item without a parent that is at placement index 0 - that is our home page that we want to redirect to
         const firstMenuItemId : number | undefined = menuItems.filter(mi => !mi.hasParent && mi.index == 0).at(0)?.id
-        router.push("/menuId/" + firstMenuItemId + "?versionId=" + versionId);
+        router.push("/menuId/" + firstMenuItemId + (versionId == 0 ? "" : "?versionId=" + versionId));
     }
     return (
             <div className="container">
